85d606979d8268bd41e72ce6c00a7b2f69f66464
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-08 23:18:58 +0530

    Remove Brackets from Algebra Expression

---
+++ removeBrackets.java
@@ -0,0 +1,9 @@
+public class removeBrackets {
+    static String solve(String input) {
+        return input.replaceAll("[(),{}]", "");
+    }
+    public static void main(String[] args) {
+        String input = "a+((b-c)+d)";
+        System.out.println(solve(input));
+    }
+}
