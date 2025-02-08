de4d1485dbf100728a94ba2da6e86d830d48fe4d
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-08 23:10:14 +0530

    Sum of Numbers in String

---
+++ sumOfNum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class sumOfNum {
+    static int sumOfNumbers(String str) {
+        int sum = 0, num = 0;
+        for (char ch : str.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            } else {
+                sum += num;
+                num = 0;
+            }
+        }
+            sum += num;
+            return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = sc.nextLine();
+        sc.close();
+        System.out.println(sumOfNumbers(str));
+    }
+}
