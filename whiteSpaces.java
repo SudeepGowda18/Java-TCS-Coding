cd7bbf3fddc920ef37b5426fd9d871ef21c3bba7
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-08 11:35:12 +0530

    Remove spaces

---
+++ whiteSpaces.java
@@ -0,0 +1,19 @@
+public class whiteSpaces {
+    static String modify(String s) {
+        int n = s.length();
+        int count = 0;
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) != ' ') {
+                res.append(s.charAt(i));
+            } else {
+                count++;
+            }
+        }
+        return res.toString();
+    }
+        public static void main(String[] args) {
+            String s = "su dee p g o wd a";
+            System.out.println(modify(s));
+        }
+    }
