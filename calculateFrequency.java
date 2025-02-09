95358623f5d13832262f68de61bef31dfd2107c9
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-09 15:01:50 +0530

    Frequency of characters in a String

---
+++ calculateFrequency.java
@@ -0,0 +1,21 @@
+public class calculateFrequency {
+    public static void printFrequency(String str) {
+        int[] freq = new int[26];
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                freq[ch - 'a']++;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                System.out.println((char) (i + 'a')+ "->" + freq[i]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String str = "sudeep gowda";
+        printFrequency(str);
+    }
+}
