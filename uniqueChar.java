2816c6d81030c0d07d1022cf35be973598afb896
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-09 12:30:15 +0530

    Index 1 Non repeating characters

---
+++ uniqueChar.java
@@ -0,0 +1,18 @@
+public class uniqueChar {
+    static int uniqueChar(String s) {
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[(ch) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1)
+                ;
+            return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        String s = "sudeep";
+        System.out.println(uniqueChar(s));
+    }
+}
