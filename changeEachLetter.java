b485af8dcb328db63cb4988adb327e5d6e4a3d86
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-10 19:48:59 +0530

    s

---
+++ changeEachLetter.java
@@ -0,0 +1,18 @@
+public class changeEachLetter {
+    static String modify(String str) {
+        StringBuilder res = new StringBuilder(str);
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                res.setCharAt(i, Character.toUpperCase(ch));
+            } else if (Character.isUpperCase(ch)) {
+                res.setCharAt(i, Character.toLowerCase(ch));
+            }
+        }
+        return res.toString();
+    }
+    public static void main(String[] args) {
+        String str = "sUDEEP gOWDA";
+        System.out.println(modify(str));
+    }
+}
