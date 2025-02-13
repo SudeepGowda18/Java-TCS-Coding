6beab9158bd17e5bea8d6dfa7bd9c998f7432a2c
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-13 22:23:35 +0530

    a

--- printEvenDigits.java
+++ printEvenDigits.java
@@ -1,13 +1,19 @@
 import java.util.*;
 
 public class printEvenDigits {
-    public static void evenFrequency(int arr) {
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        for (int num : ) {
+    public static void evenFrequency(int[] arr) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : arr) {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-        for(freq){
-
+        for (HashMap.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            if (entry.getValue() % 2 == 0)
+                System.out.print(entry.getKey() + " ");
         }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5,4,1,2,6,7};
+        evenFrequency(arr);
     }
 }
