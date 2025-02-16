6590f0f3aa16441b793879f8f0826eb9620fbe31
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-16 15:06:51 +0530

    a

---
+++ SmallestFrequentChar.java
@@ -0,0 +1,21 @@
+public class SmallestFrequentChar {
+    public static char findCount(String s, int p) {
+        s = s.toLowerCase();
+        int[] freq = new int[26];
+        for (int ch : s.toCharArray()) {
+            if(ch >= 'a' && ch <= 'z')
+            freq[ch - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] >= p) {
+                return (char) (i + 'a');
+            }
+        }
+        return '-';
+    }
+    public static void main(String[] args) {
+        String s = "sudeep gowda";
+        int p = 1;
+        System.out.println(findCount(s, p));
+    }
+}
