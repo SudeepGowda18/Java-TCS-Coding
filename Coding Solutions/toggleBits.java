8b3c2cacfb052ab7f92b984755690686f6842952
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-20 23:04:08 +0530

    Update toggleBits.java

--- Coding Solutions/toggleBits.java
+++ Coding Solutions/toggleBits.java
@@ -11,6 +11,7 @@ public class toggleBits {
         }
         return res.toString();
     }
+    //Main Method
     public static void main(String[] args) {
         int num = 75;
         System.out.println("Original "+ Integer.toBinaryString(num));
