ab7af61f808a1ac2e7fa2ac894b77b1d558e2322
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-22 16:40:39 +0530

    Add files via upload

---
+++ Coding Solutions/LargestValidNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class LargestValidNumber {
+    public static int findValidNumber(String str){
+        String[] words = str.split("[^\\d+]");
+        List<Integer> validNumbers = new ArrayList<>();
+        for(String word : words){
+            if(!word.isEmpty() && !word.contains("9")){
+                validNumbers.add(Integer.parseInt(word));
+            }
+        }
+        return validNumbers.isEmpty() ? -1 : Collections.max(validNumbers);
+    }
+    public static void main(String[] args) {
+        String str = "My name is Sudeep@ 54,88, 99 and sudeep@90";
+        System.out.println(findValidNumber(str));
+    }
+}
