1e211f07a809c585805ac70ebe240814fd705914
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-23 20:25:26 +0530

    a

---
+++ PeakElement2d.java
@@ -0,0 +1,33 @@
+public class PeakElement2d {
+    public static void findValidPeak(int[][] mat) {
+        int rows = mat.length;
+        int cols = mat[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                boolean isPeak = true;
+                //Left
+                if (j > 0 && mat[i][j] < mat[i][j - 1])
+                    isPeak = false;
+                //Right
+                if (j < cols - 1 && mat[i][j] < mat[i][j + 1])
+                    isPeak = false;
+                //Top
+                if (i > 0 && mat[i][j] < mat[i - 1][j])
+                    isPeak = false;
+                //Down
+                if (i < rows - 1 && mat[i][j] < mat[i + 1][j])
+                    isPeak = false;
+                if (isPeak)
+                    System.out.println("Peak found at : (" + i + "," + j + ")"+ " = " + mat[i][j]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[][] mat = {
+                {10,20,15},
+                {21,13,14},
+                {7,16,32}
+        };
+        findValidPeak(mat);
+    }
+}
