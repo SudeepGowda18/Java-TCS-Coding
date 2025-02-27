d1b6cf98bef87eddc2460595dddb3769617e9130
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-27 19:40:17 +0530

    Find the Sum of fibofor given Value

---
+++ SumOfFibonacci.java
@@ -0,0 +1,21 @@
+public class SumOfFibonacci {
+    public static int sumOfFibo(int n) {
+        if (n == 1)
+            return 0;
+        int first = 0;
+        int second = 1;
+        int sum = first + second;
+        int next = first + second;
+        for (int i = 2; i < n; i++) {
+            next = first + second;
+            sum += next;
+            first = second;
+            second = next;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(sumOfFibo(n));
+    }
+}
