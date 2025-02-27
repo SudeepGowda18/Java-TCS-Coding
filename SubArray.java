4a666bf67f6838c21d1dd3b1d212219fe3ba9a0a
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-02-27 14:26:25 +0530

    cvi

--- SubArray.java
+++ SubArray.java
@@ -1,7 +1,6 @@
 public class SubArray {
     public static void findSubArray(int[] arr, int target) {
         int count = 0;
-        // System.out.println("Output : ");
         for (int start = 0; start < arr.length; start++) {
             int sum = 0;
             for (int end = start; end < arr.length; end++) {
@@ -19,14 +18,14 @@ public class SubArray {
         System.out.print("[" + start + "..." + end + "] - {");
         for (int i = start; i <= end; i++) {
             System.out.print(arr[i]);
-            if (i != end)
-                System.out.print(", ");
+            if(i != end)
+            System.out.print(",");
         }
         System.out.println("}");
     }
-    public static void main(String[] args) {
-        int[] arr = { 3, 4, -7, 1, 3, 3, 1, -4 };
-        int target = 7;
-        findSubArray(arr, target);
-    }
+        public static void main(String[] args) {
+            int[] arr = { 3, 4, -7, 1, 3, 3, 1, -4 };
+            int target = 7;
+            findSubArray(arr, target);
+        }
 }
