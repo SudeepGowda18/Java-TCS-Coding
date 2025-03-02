07e533cfc87c126d22c3a5714de6cd8239225a6d
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-02 19:34:15 +0530

    Number divisible by 9

---
+++ WareHouse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class WareHouse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n = sc.nextInt();
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit;
+            n /= 10;
+        }
+        if (sum % 9 == 0) {
+            System.out.println("Number is divisible by 9");
+        } else {
+            System.out.println("Number is not divisible by 9");
+        }
+    }
+}
