b1a36be83d9571ee8858fa11d345235a1c13e738
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-03 13:33:36 +0530

    Move zeros to End

---
+++ MoveZerosToEnd.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MoveZerosToEnd {
+    public static void moveZerosToEnd(int[] arr, int n) {
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+            while (index < n) {
+                arr[index] = 0;
+                index++;
+            }
+            System.out.println("Final Output is : ");
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number of Digit");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the values");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        moveZerosToEnd(arr, n);
+        sc.close();
+    }
+}
