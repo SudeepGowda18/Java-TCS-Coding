e5383fb01e34fac62b8b0c4b86fd0dd227888d44
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-03 16:35:51 +0530

    Count Sundays.java

---
+++ CountSundays.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class CountSundays {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the StartDay");
+        // String startDay = sc.next().toLowerCase();
+        // System.out.println("Enter the Number of days left in a month");
+        // int n = sc.nextInt();
+        // Map<String, Integer> dayMap = new HashMap<>();
+        // dayMap.put("sun", 0);
+        // dayMap.put("mon", 1);
+        // dayMap.put("tue", 2);
+        // dayMap.put("wed", 3);
+        // dayMap.put("thu", 4);
+        // dayMap.put("fri", 5);
+        // dayMap.put("sat", 6);
+        // int startIndex = dayMap.get(startDay);
+        // int firstSunday = (7 - startIndex) % 7;
+        // int count = 0;
+        // for (int i = firstSunday; i <= n; i += 7) {
+        //     count++;
+        // }
+        // System.out.println("Number of days : " + count);
+        Scanner sc = new Scanner(System.in);
+        String[] arr = { "mon", "tue", "wed", "thur", "fri", "sat", "sun" };
+        String str = sc.next();
+        int n = sc.nextInt();
+        int i = 0;
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i].equals(str)) {
+                break;
+            }
+        }
+        int res = 1;
+        int rem = 6 - i;
+        n = n - rem;
+        if (n > 0) {
+            res += n / 7;
+        }
+        System.out.println(res);
+    }
+}
