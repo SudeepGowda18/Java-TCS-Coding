d393b1a57efbc0adffd094df3fa16aacbae82e3c
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-03 22:26:25 +0530

    Sort risk level

---
+++ SortRiskLevel.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SortRiskLevel {
+    public static void sortRisk(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number of Digits");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the Values");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sortRisk(arr);
+        for(int num : arr){
+            System.out.print(num);
+        }
+    }
+}
