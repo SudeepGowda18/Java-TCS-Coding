a471cacf9a000a9d8b003b7519d49e94cecd82c9
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-03 14:48:59 +0530

    Toggle bits

---
+++ ToggleBits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ToggleBits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Decimal");
+        int n = sc.nextInt();
+        int msb = 1;
+        while (msb <= n) {
+            msb <<= 1;
+        }
+        int mask = msb - 1;
+        int result = n ^ mask;
+        System.out.println("Output : " + result);
+    }
+}
