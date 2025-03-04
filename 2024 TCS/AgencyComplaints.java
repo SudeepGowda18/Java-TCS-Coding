33cf0f7f4893b0a25da4df8aa88a6187a4e49f8e
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-04 23:08:43 +0530

    Update AgencyComplaints.java

--- 2024 TCS/AgencyComplaints.java
+++ 2024 TCS/AgencyComplaints.java
@@ -2,42 +2,19 @@ import java.util.Scanner;
 
 public class AgencyComplaints {
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter the Number");
-        // int N = sc.nextInt();
-        // System.out.println("Enter the Value R");
-        // int R = sc.nextInt();
-        // sc.close();
-        // int sum = 0;
-        // while (N > 0) {
-        //     int digit = N % 10;
-        //     sum += digit;
-        //     N /= 10;
-        // }
-        // int result = sum * R;
-        // while (result >= 10) {
-        //     int temp = 0;
-        //     while (result > 0) {
-        //         temp += result % 10;
-        //         result /= 10;
-        //     }
-        //     result = temp;
-        // }
-        // //Main Method
-        // System.out.println("Output : "+result);
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Value of L");
-        int n = sc.nextInt();
-        System.out.println("Enter the Value of R");
-        int r = sc.nextInt();
+        System.out.println("Enter the Number");
+        int N = sc.nextInt();
+        System.out.println("Enter the Value R");
+        int R = sc.nextInt();
         sc.close();
         int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
+        while (N > 0) {
+            int digit = N % 10;
             sum += digit;
-            n /= 10;
+            N /= 10;
         }
-        int result = sum * r;
+        int result = sum * R;
         while (result >= 10) {
             int temp = 0;
             while (result > 0) {
@@ -46,6 +23,7 @@ public class AgencyComplaints {
             }
             result = temp;
         }
-        System.out.println("Output : " + result);
+        //Main Method
+        System.out.println("Output : "+result);
     }
 }
