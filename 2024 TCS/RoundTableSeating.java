31f4b29d86636ba8f859a6c2fefcfaefd95b4c4c
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-04 23:10:34 +0530

    Update RoundTableSeating.java

--- 2024 TCS/RoundTableSeating.java
+++ 2024 TCS/RoundTableSeating.java
@@ -7,7 +7,7 @@ public class RoundTableSeating {
             fact *= i;
         }
         return fact;
-    }
+    } 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
