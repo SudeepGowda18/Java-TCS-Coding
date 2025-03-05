c252cf567d0a3b9dd1e1798a02c4d87a4e152f83
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-05 22:41:09 +0530

    Update AgencyComplaints.java

--- 2024 TCS/AgencyComplaints.java
+++ 2024 TCS/AgencyComplaints.java
@@ -9,7 +9,7 @@ public class AgencyComplaints {
         int R = sc.nextInt();
         sc.close();
         int sum = 0;
-        while (N > 0) {
+        while (N > 0) { 
             int digit = N % 10;
             sum += digit;
             N /= 10;
