7ab8480b2dcfa4e0bfa67668344ef43959dfda4d
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-06 22:50:47 +0530

    Update WareHouse.java

--- 2024 TCS/WareHouse.java
+++ 2024 TCS/WareHouse.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class WareHouse { 
     public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Number");
+        System.out.println("Enter the Number");;
         int n = sc.nextInt(); 
         sc.close();
         int sum = 0;  
