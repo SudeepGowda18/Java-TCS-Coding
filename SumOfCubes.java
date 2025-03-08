65becf5f1d9f4719c7b75e58c6090c6df70dc0f2
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-08 22:04:13 +0530

    Sum of cubes

---
+++ SumOfCubes.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumOfCubes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int sum = 0;
+        for (int i = start; i <= end; i++){
+        sum += i * i * i;
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
