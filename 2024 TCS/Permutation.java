6b3bc2df837ad7fe3aaef02cffdee91380b9147e
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-11 23:35:15 +0530

    Update Permutation.java

--- 2024 TCS/Permutation.java
+++ 2024 TCS/Permutation.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Permutation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of digits");
+        System.out.println("Enter the number of digits"); 
         int n = sc.nextInt();
         int[] p_inverse = new int[n + 1];
         System.out.println("Enter the values");
