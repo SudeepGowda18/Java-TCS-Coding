3a10ab3366916d10b3326ceb62560ba66f0ec580
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-03-17 20:14:49 +0530

    Update AgencyComplaintss.java

--- 2024 TCS/AgencyComplaintss.java
+++ 2024 TCS/AgencyComplaintss.java
@@ -1,7 +1,7 @@
 import java.util.Scanner; 
  
 public class AgencyComplaintss {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {  
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Number");
         int N = sc.nextInt();
