4685289b67cdbc05fc4c2717b6c8d0f9e2d6d745
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-04-02 12:50:05 +0530

    Update MaxEfficiency.java

--- 2024 TCS/MaxEfficiency.java
+++ 2024 TCS/MaxEfficiency.java
@@ -18,7 +18,7 @@ public class MaxEfficiency {
                 max2 = max1;
                 max1 = num;
             } else if (num > max2) {
-                max3 = max2;
+                max3 = max2; 
                 max2 = num;
             } else if (num > max3) {
                 max3 = num;
