04afe549fbdd426647d21aed40d17e5ae39730d0
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-04-02 12:49:55 +0530

    Update MaxEfficiency.java

--- 2024 TCS/MaxEfficiency.java
+++ 2024 TCS/MaxEfficiency.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MaxEfficiency {
+public class MaxEfficiency { 
     public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in); 
         System.out.println("Enter the Number of Employees"); 
