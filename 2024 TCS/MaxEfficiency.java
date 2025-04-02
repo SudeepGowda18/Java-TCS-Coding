70992fd989f4937b783f1104ea818e57983882ee
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-04-02 12:49:06 +0530

    Update MaxEfficiency.java

--- 2024 TCS/MaxEfficiency.java
+++ 2024 TCS/MaxEfficiency.java
@@ -6,7 +6,7 @@ public class MaxEfficiency {
         System.out.println("Enter the Number of Employees");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        System.out.println("Enter employee efficiency");
+        System.out.println("Enter employee efficiency"); 
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
