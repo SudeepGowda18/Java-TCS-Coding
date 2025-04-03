c810e8c3bfe5cfc0d61b2cac68fb5ac1aa08f961
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-04-03 12:34:02 +0530

    Update CountSundays.java

--- 2024 TCS/CountSundays.java
+++ 2024 TCS/CountSundays.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class CountSundays {
+public class CountSundays { 
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the StartDay"); 
